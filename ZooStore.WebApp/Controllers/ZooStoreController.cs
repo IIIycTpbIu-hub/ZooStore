f1649ded9d5588a9dbc3f5f170b00e117551ef3d
Andrew Raznopolov <razandrey95@gmail.com>
2020-11-18 15:07:19 +0400

    Первые шаги в структуре проекта

---
+++ ZooStore.WebApp/Controllers/ZooStoreController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ZooStore.WebApp.Controllers
+{
+    class ZooStoreController
+    {
+    }
+}
---
+++ ZooStore.WebApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZooStore.WebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
